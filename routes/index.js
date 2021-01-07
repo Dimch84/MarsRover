e1887b025c9cdc0a1b2f921b0075f67c6de2dce4
unknown <dmitry.shalymov@gmail.com>
2021-01-07 11:05:44 +0300

    initial page changed

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+//});
+
+//console.log(__dirname);
+
+router.get('/', function(req, res) {
+  res.sendFile(__dirname + '\\main.html');
 });
 
 module.exports = router;
