132be9c25e652e7a81b69f483131956bea98902c
Egor Postnikov <ea-post134@yandex.ru>
2021-01-11 22:15:34 +0300

    change

--- script.ts
+++ script.ts
@@ -1,25 +1,18 @@
 const canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("canvas")
 let dataWidth;
 let dataHeight;
-let size;
+let squareSize;
 let ctx;
 let canvasWidth;
 let canvasHeight;
 let curArrData;
 
 
-function saveMap(): void {
+async function saveMap() {
     const field: HTMLTextAreaElement = <HTMLTextAreaElement>document.getElementById("map_area")
 
-    const str: String = String(field.value)
-    if (str.match(/[^\d\n\s]/gi) != null) {
-        alert("Во вводе не должно быть ничего кроме цифр")
-        return
-    }
+    const arrData = await JSON.parse(field.value)
 
-    const arrData = str.split("\n").map(function (elem: String): Array<String> {
-        return elem.split(" ")
-    })
     for (let line of arrData) {
         console.log(line)
         if (line.length != arrData[0].length) {
@@ -32,124 +25,112 @@ function saveMap(): void {
 
 
 function applyDataToMap(arrData: Array<String>[]) {
+    canvas.hidden = false
     curArrData = arrData
 
     dataWidth = arrData[0].length
     dataHeight = arrData.length
 
-    size = Math.min(innerWidth * 0.95 / dataWidth, innerHeight * 0.95 / dataHeight)
+    squareSize = Math.min(innerWidth * 0.95 / dataWidth, innerHeight * 0.95 / dataHeight)
     ctx = canvas.getContext('2d')
-    canvasWidth = dataWidth * size
-    canvasHeight = dataHeight * size
+    canvasWidth = dataWidth * squareSize
+    canvasHeight = dataHeight * squareSize
 
-    canvas.setAttribute("width", String(dataWidth * size))
-    canvas.setAttribute("height", String(dataHeight * size))
+    canvas.setAttribute("width", String(dataWidth * squareSize))
+    canvas.setAttribute("height", String(dataHeight * squareSize))
 
     console.log(dataHeight);
     console.log(dataWidth)
 
-    clearCanvas();
+    refreshCanvas();
 
 
     (<HTMLButtonElement>document.getElementById("run_btn")).disabled = false
 }
 
-function clearCanvas(): void {
+const patterns: Array<HTMLImageElement> = new Array<HTMLImageElement>(6);
+
+function refreshCanvas(): void {
     ctx.fillStyle = "#ffe4ff";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     ctx.fillStyle = "#000000";
-    ctx.font = Math.max(30, size / 100) + "px Arial";
-    for (let i = 0; i < dataHeight; i++) {
-        for (let j = 0; j < dataWidth; j++) {
-            ctx.fillText(curArrData[i][j], (j + 0.5) * size, (i + 0.5) * size, size)
-        }
-    }
-    for (let i = 0; i < dataWidth; i++) {
-        ctx.beginPath();
-        ctx.moveTo(i * size, canvasHeight);
-        ctx.lineTo(i * size, 0);
-        ctx.strokeStyle = "red";
-        ctx.stroke();
-    }
+    ctx.font = Math.max(30, squareSize / 100) + "px Arial";
 
     for (let i = 0; i < dataHeight; i++) {
-        ctx.beginPath();
-        ctx.moveTo(canvasWidth, i * size);
-        ctx.lineTo(0, i * size);
-        ctx.strokeStyle = "red";
-        ctx.stroke();
+        for (let j = 0; j < dataWidth; j++) {
+            ctx.fillText(curArrData[i][j], (j + 0.5) * squareSize, (i + 0.5) * squareSize, squareSize)
+            ctx.drawImage(patterns[curArrData[i][j]], j * squareSize, i * squareSize, squareSize, squareSize)
+        }
     }
+
 }
 
 function initCanvas(): void {
     const size = window.innerHeight
     canvas.setAttribute("width", String(size * 0.9))
     canvas.setAttribute("height", String(size * 0.95))
-
-    const ctx = canvas.getContext('2d');
-
-    const grad = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
-    grad.addColorStop(0, "magenta");
-    grad.addColorStop(0.5, "blue");
-    grad.addColorStop(1.0, "red");
-
-    ctx.fillStyle = grad;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    ctx.fillStyle = null
-    ctx.fillText("Big smile!", 10, 90);
 }
 
-function runWay(): void {
-    clearCanvas()
+async function runWay() {
+    refreshCanvas()
 
     const pathField = <HTMLTextAreaElement>document.getElementById("path_area")
-    const data = pathField.value
+    const pathObject = await JSON.parse(pathField.value)
+    console.log(pathObject)
 
+    const data = pathObject.path;
     const ctx = canvas.getContext('2d')
     ctx.beginPath();
-    ctx.moveTo(size / 2, size / 2)
+    ctx.moveTo(squareSize * (pathObject.start.x - 0.5), squareSize * (pathObject.start.y - 0.5))
 
-    var curX = size / 2
-    var curY = size / 2
+    var curX = squareSize / 2
+    var curY = squareSize / 2
 
     const deltaX = {
-        'U': 0,
-        'D': 0,
-        'R': +1,
-        'L': -1
+        'UP': 0,
+        'DOWN': 0,
+        'RIGHT': +1,
+        'LEFT': -1
     };
 
     const deltaY = {
-        'U': -1,
-        'D': +1,
-        'R': 0,
-        'L': 0
+        'UP': -1,
+        'DOWN': +1,
+        'RIGHT': 0,
+        'LEFT': 0
     };
     for (const c of data) {
-        curX += deltaX[c] * size
-        curY += deltaY[c] * size
+        curX += deltaX[c] * squareSize
+        curY += deltaY[c] * squareSize
         ctx.lineTo(curX, curY)
         if (curX > canvasWidth || curY > canvasHeight || curX < 0 || curY < 0) {
             alert("You've lost")
             break;
         }
     }
-    var a = ctx.lineWidth
+    const savedWidth = ctx.lineWidth;
     ctx.lineWidth = 10;
-    ctx.strokeStyle = "black"
+    ctx.strokeStyle = "white"
 
-    ctx.arc(curX, curY, size / 6, 0, 2 * Math.PI);
+    ctx.arc(curX, curY, squareSize / 6, 0, 2 * Math.PI);
     ctx.stroke();
 
     ctx.beginPath()
-    ctx.arc(curX, curY, size / 6, 0, 2 * Math.PI);
+    ctx.arc(curX, curY, squareSize / 6, 0, 2 * Math.PI);
     ctx.fill();
-    ctx.lineWidth = a;
+    ctx.lineWidth = savedWidth;
     ctx.stroke()
 }
 
+function loadPictures() {
+    for (let i = 0; i < 6; i++) {
+        patterns[i] = new Image();
+        patterns[i].src = "patterns/type" + (i + 1).toString() + ".png";
+    }
+}
+
 function initView(): void {
-    initCanvas()
+    initCanvas();
+    loadPictures();
 }
